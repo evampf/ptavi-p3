9837d073a0ac80daa91fd92b22b0321e821c09a3
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-Eva.local>
2016-10-06 19:02:12 +0200

    añadido def get_tag, con errores

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -6,14 +6,36 @@ from xml.sax.handler import ContentHandler
 
 class SmallSMILHandler(ContentHandler):
 
-	def __init__ (self):
-		self.DicEtiquetas: {
-			'root-layout' = ['width','height','background-color']
-			'region' = ['id','top','bottom','left','rigth']
-			'img' = ['src','region','begin','dur']
-			'audio' = ['src','begin','dur']
-			'textstream' = ['src','region']
-		}
-		self.atrib: []
+    def __init__ (self):
+        self.DicEtiquetas = {
+            'root-layout': ['width','height','background-color'],
+            'region': ['id','top','bottom','left','rigth'],
+            'img': ['src','region','begin','dur'],
+            'audio': ['src','begin','dur'],
+            'textstream': ['src','region']
+        }
+        self.lista = []
+
+    def startElement(self, name, attrs):
+    
+        if name in self.DicEtiquetas:
+            Dicc = {}
+            for atributo in self.DicEtiquetas[name]:
+                Dicc[atributo] = attrs.get(atributo, "")
+            Diccname = {name: dicc}
+        self.lista.append(Dicc)
 
 
+    def get_tags(self):
+        return self.lista
+    
+
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    print (cHandler.get_tags())

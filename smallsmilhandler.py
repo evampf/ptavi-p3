8b553107ecea183bb1fe6b6fdf5cd2b68e5afcac
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-Eva.local>
2016-10-06 18:29:15 +0200

    Initial commit

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class SmallSMILHandler(ContentHandler):
+
+	def __init__ (self):
+		self.DicEtiquetas: {
+			'root-layout' = ['width','height','background-color']
+			'region' = ['id','top','bottom','left','rigth']
+			'img' = ['src','region','begin','dur']
+			'audio' = ['src','begin','dur']
+			'textstream' = ['src','region']
+		}
+		self.atrib: []
+
 

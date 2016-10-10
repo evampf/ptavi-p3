37752205bd23abf21c11b3e149ad3f184ee1d94d
Eva Maria Perez <evaperez.fernandez@gmail.com>
2016-10-10 18:01:22 +0200

    pasado pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,21 +4,18 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
+    def __init__(self):
         self.lista = []
         self.DicEtiquetas = {
-            'root-layout': ['width','height','background-color'],
-            'region': ['id','top','bottom','left','rigth'],
-            'img': ['src','region','begin','dur'],
-            'audio': ['src','begin','dur'],
-            'textstream': ['src','region']
-            }
-        
+            'root-layout': ['width', 'height', 'backgroundcolor'],
+            'region': ['id', 'top', 'bottom', 'left', 'rigth'],
+            'img': ['src', 'region', 'begin', 'dur'],
+            'audio': ['src', 'begin', 'dur'], 'textstream': ['src', 'region']}
 
     def startElement(self, name, attrs):
-    
         if name in self.DicEtiquetas:
             dicc = {}
             for atributo in self.DicEtiquetas[name]:
@@ -26,10 +23,8 @@ class SmallSMILHandler(ContentHandler):
             diccname = {name: dicc}
             self.lista.append(diccname)
 
-
     def get_tags(self):
         return self.lista
-    
 
 if __name__ == "__main__":
     """

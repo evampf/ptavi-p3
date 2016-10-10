45de21969338196245d349c612119821342c0feb
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-Eva.local>
2016-10-10 12:33:44 +0200

    Listado ordenado + JSON

--- karaoke.py
+++ karaoke.py
@@ -2,12 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import json
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 
-# class Karaoke(smallsmilhandler.Karaoke):
+#class Karaoke():
 
+def Crear_Json(doc):
+    with open(sys.argv[1][:-5]+".json", 'w') as outfile:
+        json.dump(doc, outfile,indent=4,separators=(',', ': ')) 
+
+
+def Formato_Lista(Lista):
+
+    doc = ""
+    for linea in Lista:
+        for name in linea:
+            doc = doc + name + "\t"
+            for name2 in linea[name]:
+                doc = doc + name2 +'=' + linea[name][name2] + '"' + "\t"
+            doc = doc + "\n"
+    print (doc)
 
 
 if __name__ == "__main__":
@@ -18,6 +34,10 @@ if __name__ == "__main__":
     try:
         parser.parse(open(sys.argv[1]))
     except:
-        sys.exit("Usage: python3 karaoke.py file.smil")
+        sys.exit('Usage: python3 karaoke.py file.smil')
+
     print(cHandler.get_tags())
+    Crear_Json(cHandler.get_tags())
+    Formato_Lista(cHandler.get_tags())
+
     
